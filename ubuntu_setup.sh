6fbbe37ce412a14bea2fc27e8ffa3b5e5ef827a1
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2018-03-21 20:14:26 +0100

    Fixed requirements & added ubuntu_setup.sh

---
+++ ubuntu_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt install portaudio19-dev libatlas-base-dev libpcre3-dev libpython-dev libpython3-dev python3-dev
+
+wget https://kent.dl.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
+tar xf swig-3.0.12.tar.gz
+cd swig-3.0.12
+./configure
+make -j4
+sudo make install
+
+cd ../snowboy/swig/Python3
+make
+cp _snowboydetect.so ../../../
