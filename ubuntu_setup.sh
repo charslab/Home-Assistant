66223a86a400597230fe9a6c808ff52bf16b4da6
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2018-03-21 21:10:55 +0100

    Added .gitmodules

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install portaudio19-dev libatlas-base-dev libpcre3-dev libpython-dev libpython3-dev python3-dev
+sudo apt install portaudio19-dev libatlas-base-dev libpcre3-dev libpython-dev libpython3-dev python3-dev mplayer
 
 wget https://kent.dl.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
 tar xf swig-3.0.12.tar.gz
